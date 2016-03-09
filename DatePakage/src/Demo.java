73e1d0aef8393a206a84bb26e6b7b69644443514
kidistgondel <yekid@DESKTOP-T04MQ2Q>
2016-03-09 09:06:37 -0700

    this is the first change

---
+++ DatePakage/src/Demo.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Date date = new Date();
+		System.out.println(date.toString());
+		System.out.println(date.getMonth()+1);
+
+		System.out.println(date.getYear()+1900);
+		System.out.println(date.getDate());
+	}
+
+}
